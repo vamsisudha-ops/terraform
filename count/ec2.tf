9c67b4345ace1a8f941d04408098e91be28cc8ac
vamsisudha-ops <vamsisudha49@gmail.com>
2026-04-29 18:04:54 +0530

    terraform

---
+++ count/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+    count = 4
+    #count = length(var.instances)
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_everyhost.id]
+    
+    tags = {
+        Name = var.instances[count.index]
+        Terraform = "true"
+    }
+}
+
+resource "aws_security_group" "allow_everyhost" {       #here allow_everyhost is Terraform Reference 
+  name   = "allow-everyhost"   # here allow-everyhost is Security group name 
+
+  egress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  ingress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  tags = {
+    Name = "allow-everyhost"       # here allow-everyhost is for user-display in AWS Console 
+  }
+
+}
---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id" {
+    default = "Z045918523UHFS3WTXGUZ"
+}
+
+variable "domain_name" {
+    default = "daws86sd.fun"
+}
