90fba9659e120ebecea42eccb451445f902d8233
vamsisudha-ops <vamsisudha49@gmail.com>
2026-07-23 15:46:07 +0530

    lifecycle

--- lifecycle/ec2.tf
+++ lifecycle/ec2.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "terraform" {
 # }
 
 resource "aws_security_group" "lifecycle" {
-  name   = "lifecycle"
+  name   = "lifecycle_change"
 
   egress {
     from_port        = 0 # from port 0 to to port 0 means all ports
