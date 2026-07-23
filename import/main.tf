0182746408239a093453f18072702a84e7f28676
vamsisudha-ops <vamsisudha49@gmail.com>
2026-07-23 15:05:57 +0530

    import

--- import/main.tf
+++ import/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "demo" {
   instance_type = "t3.micro"
-  ami           = "ami-09c813fb71547fc4f"
+  ami           = "ami-0b826bb6d96d2afe4"
   vpc_security_group_ids = [
-    "sg-07c8acf3fa6b923fa"
+    "sg-0cb821092eec51d4c"
   ]
   tags = {
     Name = "import-demo-change"
